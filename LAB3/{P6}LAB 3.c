e8fee5d6a0d79a3b580b7a1e6dd399fd229aa916
tasneem200412 <tasneemhossain208@gmail.com>
2024-11-15 01:14:22 +0600

    HEllo WOrld

---
+++ LAB3/{P6}LAB 3.c
@@ -0,0 +1,3 @@
+/*N  pieces of papers are
+necessary to prepare one book, how many books can be prepared by M
+(M>N)  pieces of papers?*/
---
+++ LAB3/{P8}LAB 3.c
@@ -0,0 +1,3 @@
+/*There are X grams of rice at
+home. If we eat Y  (Y<X)
+grams of rice every day, when the rice will be finish?*/
